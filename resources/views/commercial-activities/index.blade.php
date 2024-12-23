829308e58dab2ce61468f80d77b15918830029d0
davidebenzo <davidebenzo@gmail.com>
2024-12-23 23:43:09 +0100

    apikey env

--- resources/views/commercial-activities/index.blade.php
+++ resources/views/commercial-activities/index.blade.php
@@ -4,7 +4,8 @@
             Elenco delle tue attività
         </h2>
     </x-slot>
-   
+    
+    <script src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_MAPS_API_KEY')}}"></script>
     <script>
         function initMap() {
             const map = new google.maps.Map(document.getElementById('map'), {
